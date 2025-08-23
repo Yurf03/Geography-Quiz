74f2a2b6c81558cd5dd3b41ae1f4bfad2901543b
Lino <jovanizuniga1@gmail.com>
2025-08-22 20:45:08 -0700

    final touches

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-const startPage = document.getElementById("#startContainer");
-const startButton = document.getElementById("#startButton");
-const quizPage = document.getElementById("#quizContainer");
-const questionElement = document.getElementById("#questionContainer");
-const optionsButtons = document.getElementById("#optionsContainer");
-const nextButton = document.getElementById("#nextButton");
-const scorePage = document.getElementById("#scoreContainer");
-const scoreResult = document.getElementById("#score");
-const restartButton = document.getElementById("#restartButton");
+const startPage = document.getElementById("startContainer");
+const startButton = document.getElementById("startButton");
+const quizPage = document.getElementById("quizContainer");
+const questionElement = document.getElementById("questionContainer");
+const optionsButtons = document.getElementById("optionsContainer");
+const nextButton = document.getElementById("nextButton");
+const scorePage = document.getElementById("scoreContainer");
+const scoreResult = document.getElementById("score");
+const restartButton = document.getElementById("restartButton");
 
-let shuffledQuestions, currentQuestionIndex, score;
+
 
 const quizData = [
     {
-        question: "what is the tallest mountain in the world?"
+        question: "What is the tallest mountain in the world?",
         options: [
             { text: "K2", correct: false },
             { text: "Mount Everest", correct: true },
             { text: "Manaslu", correct: false },
             { text: "Kamet", correct: false }
-        ]
-    }
+        ],
+    },
     {
-        question: "what is the worlds largest desert?"
+        question: "What is the world's largest desert?",
         options: [
             { text: "Sahara Desert", correct: false },
             { text: "Gobi Desert", correct: false },
             { text: "Syrian Desert", correct: false },
             { text: "Antarctic Desert", correct: true }
-        ]
-    }
+        ],
+    },
     {
-        question: "what is the driest continent on Earth?"
+        question: "What is the driest continent on Earth?",
         options: [
             { text: "Africa", correct: false },
             { text: "Asia", correct: false },
             { text: "Antarctica", correct: true },
             { text: "Europe", correct: false }
-        ]
-    }
+        ],
+    },
     {
-        question: "In what ocean is the Bermuda Triangle located?"
+        question: "In what ocean is the Bermuda Triangle located?",
         options: [
             { text: "Atlantic Ocean", correct: true },
             { text: "Pacific Ocean", correct: false },
@@ -48,3 +48,89 @@ const quizData = [
         ]
     }
 ]
+
+function loadQuestions() {
+    startPage.style.display = "none";
+    quizPage.style.display = "block";
+    shuffledQuestions = quizData.sort(() => Math.random() - 0.5);
+    currentQuestionIndex = 0;
+    score = 0;
+    showNextQuestion();
+}
+function showNextQuestion() {
+    resetState();
+    displayQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function displayQuestion(question) {
+    questionElement.innerText = question.question;
+    question.options.forEach(option => {
+        const button = document.createElement("button");
+        button.innerText = option.text;
+        button.classList.add("btn");
+        button.style.fontSize = "15px";      
+        button.style.width = "30%"; 
+        button.style.padding = "15px";
+        if (option.correct) {
+            button.dataset.correct = option.correct;
+        }
+        button.addEventListener("click", selectOption);
+        
+        optionsButtons.appendChild(button);
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (optionsButtons.firstChild) {
+        optionsButtons.removeChild(optionsButtons.firstChild);
+    }
+}
+
+function selectOption(i) {
+    const selectedButton = i.target;
+    const isCorrect = selectedButton.dataset.correct === "true";
+    if (isCorrect) {
+        selectedButton.classList.add("correct");
+        score++;
+    } else {
+        selectedButton.classList.add("incorrect");
+    }
+    Array.from(optionsButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.style.backgroundColor = "Orange"; 
+            button.style.color = "white";
+        }
+        button.disabled = true;
+    });
+    if (!isCorrect) {
+        selectedButton.style.backgroundColor = "purple"; 
+        selectedButton.style.color = "white";
+    }
+    nextButton.style.display = "block";
+    nextButton.style.marginLeft = "auto";
+    nextButton.style.marginRight = "auto";
+}
+
+function showScore() {
+    quizPage.style.display = "none";
+    scorePage.style.display = "block";
+    scoreResult.innerText = `You scored ${score} out of ${shuffledQuestions.length}!`;
+}
+
+function restartQuiz() {
+    scorePage.style.display = "none";
+    startPage.style.display = "block";
+}
+
+startButton.addEventListener("click", loadQuestions);
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < shuffledQuestions.length) {
+        showNextQuestion();
+    } else {
+        showScore();
+    }
+});
+restartButton.addEventListener("click", restartQuiz);
+
